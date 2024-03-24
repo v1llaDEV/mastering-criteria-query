915cd983d08c6cd2e7b82eaa640397cc420c3ab1
v1lla <adrianvillasecosoto@gmail.com>
2024-03-24 11:42:43 +0100

    Adding dummy values for dog entity

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO DOG (name, breed, birthday, color, vaccinated) VALUES ('Bobby', 'Labrador Retriever', '2019-05-10', 'Black', TRUE);
+INSERT INTO DOG (name, breed, birthday, color, vaccinated) VALUES ('Luna', 'Border Collie', '2020-02-15', 'White and Black', TRUE);
+INSERT INTO DOG (name, breed, birthday, color, vaccinated) VALUES ('Max', 'Golden Retriever', '2018-12-03', 'Gold', FALSE);
+INSERT INTO DOG (name, breed, birthday, color, vaccinated) VALUES ('Lucky', 'Chihuahua', '2021-08-20', 'Brown', TRUE);
+INSERT INTO DOG (name, breed, birthday, color, vaccinated) VALUES ('Rocky', 'Bulldog Francés', '2017-10-29', 'Yellow', TRUE);
